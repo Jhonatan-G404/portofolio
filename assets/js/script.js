a0ff7fc826aca2cd9b4144c0ad2e59698d29eaf5
Jhonatan-G404 <jhonatan.27092003@gmail.com>
2024-09-28 18:07:49 +0700

    memperbarui contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,25 @@ document.addEventListener("DOMContentLoaded", function () {
     .delete()
     .go();
 });
+
+document
+  .getElementById("btnKirim")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    swal({
+      title: "Kirim pesan",
+      text: "Anda yakin ingin mengirim pesan ini?",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willSend) => {
+      if (willSend) {
+        swal("Yeyy! Pesan anda berhasil Dikirim", {
+          icon: "success",
+        });
+      } else {
+        swal("Pesan Anda Dibatalkan");
+      }
+    });
+  });
